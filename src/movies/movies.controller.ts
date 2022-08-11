27de774bc13ec8e7342c471c490738bf118b9194
jun <ggprgrkjh@naver.com>
2022-08-11 21:09:09 +0900

    #2.0 Movies controller

---
+++ src/movies/movies.controller.ts
@@ -0,0 +1,30 @@
+/* eslint-disable prettier/prettier */
+import { Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+
+@Controller('movies')
+export class MoviesController {
+  @Get()
+  getAll() {
+    return 'this will return all movies';
+  }
+  @Get("/:id")
+  getOne(@Param("id") movieId:string){
+      return `this will return one movie with the id: ${movieId}`;
+  }
+  @Post()
+  create(){
+      return "This will create a movie";
+  }
+  @Delete("/:id")
+      remove(@Param('id') movieId:string){
+          return `This will delete a movie with the id: ${movieId}`
+    }
+
+  @Patch('/:id')
+  path(@Param('id') movieId: string){
+    return `This will patch a movie with the id: ${movieId}`
+}
+
+  
+
+}
