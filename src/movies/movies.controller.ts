cdd1f19e86f1460d9780ff4e1bdd58b9847579f3
jun <ggprgrkjh@naver.com>
2022-08-13 00:07:04 +0900

    #2.6 Modules and Dependency Injection

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -2,7 +2,7 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
 import { CreateMovieDto } from './dto/create-movie.dto';
 import { UpdateMovieDto } from './dto/update-movie.dto';
-import { Movie } from './entities/mvoie.entity';
+import { Movie } from './entities/movie.entity';
 import { MoviesService } from './movies.service';
 
 @Controller('movies')
--- src/movies/movies.service.ts
+++ src/movies/movies.service.ts
@@ -2,7 +2,7 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateMovieDto } from './dto/create-movie.dto';
 import { UpdateMovieDto } from './dto/update-movie.dto';
-import { Movie } from './entities/mvoie.entity';
+import { Movie } from './entities/movie.entity';
 
 @Injectable()
 export class MoviesService {
