976e1a24822486a973be22efd3797e100bdff83d
jun <ggprgrkjh@naver.com>
2022-08-13 00:22:21 +0900

    #3.1 first unit test by jest

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { Test, TestingModule } from '@nestjs/testing';
 import { MoviesService } from './movies.service';
 
@@ -15,4 +16,11 @@ describe('MoviesService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  it("should be 4",()=>{
+    expect(2+3).toEqual(5);
+  })
+
+
+
 });
